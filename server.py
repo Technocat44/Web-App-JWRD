6b7998de418d3efe73bef868171577c50701f853
jamesaqu <jamesaqu@buffalo.edu>
2022-03-19 23:42:52 -0400

    wrote some comments in the server main file about setting debug to True in the run function

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask # have to import the Flask Class from the flash moduel
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +6,7 @@ def hello_world():
    return "Hello World"
 
 if __name__ == '__main__':
-        #   host,    port, debug
-   app.run("0.0.0.0",5000, False)
+    #      host,    port, debug
+    # having debug set to True is important. The server will reload itself if the code change instead of having to do it
+    # manually. 
+   app.run("0.0.0.0",5000, True)
