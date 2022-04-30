5230d319e142780c9428b9513b979e78af3308b4
wfmarcha <wfmarcha@buffalo.edu>
2022-04-30 17:26:45 -0400

    profile pic

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # this makes the webapp folder a package and we can import it anywhere else in our code easily 
 from flask import Flask
-from webapp import create_app, views, auth, upload, images, messages, users
+from webapp import create_app, views, auth, upload, images, messages, users, account
 
 
 app = create_app()
@@ -14,6 +14,7 @@ app.register_blueprint(auth.auther, url_prefix='/')
 app.register_blueprint(upload.uploader, url_prefix='/')
 app.register_blueprint(messages.messager, url_prefix='/')
 app.register_blueprint(users.usersGiver, url_prefix='/')
+app.register_blueprint(account.accounter, url_prefix='/')
 
 if __name__ == '__main__':
     # having debug set to True is important. The server will reload itself if the code change instead of having to do it
