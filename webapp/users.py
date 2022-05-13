a82deca7fa6e85ce5e19507bdf0bfb210f7924e1
jamesaqu <jamesaqu@buffalo.edu>
2022-05-12 21:54:58 -0400

    re-adding user.py to daniils last commit

--- webapp/users.py
+++ webapp/users.py
@@ -1,30 +1,21 @@
 import json
 from flask import Blueprint, render_template,request, flash, session, blueprints
-from webapp.database import get_all_users, fetch_messages, get_user_collection_via_auth_token
+from webapp.database import get_all_users, fetch_messages, get_user_collection_via_auth_token, get_user_from_id, add_message
 import time 
 import json
-
 usersGiver = Blueprint("users", __name__)
-
 activeWebSocketConnections = []
-
-@usersGiver.route("/ws")
-def sock(ws):
-  while True:
-      activeWebSocketConnections.append()
-      data = ws.receive()
-      ws.send(data)
-      # time.sleep(.10)
-
+messageTracking = {}
+# @usersGiver.route("/ws")
+# def sock(ws):
+#   while True:
+#       activeWebSocketConnections.append()
+#       data = ws.receive()
+#       ws.send(data)
+#       # time.sleep(.10)
 @usersGiver.route('/users')
 def usersHandler():
-    # token = request.cookies.get('auth_token',-1)
-    # user = get_user_collection_via_auth_token(token)
-    # defaultPicture = "./static/images/" + user['profile_pic']
-    # if defaultPicture == None:
-    defaultPicture= 'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png'
-    print(defaultPicture,flush=True)
-    # print(123)
+    defaultPicture = 'https://www.tenforums.com/geek/gars/images/2/types/thumb_15951118880user.png'
     users = get_all_users()
     toSend = []
     for user in users:
@@ -33,9 +24,8 @@ def usersHandler():
             userSend = {}
             userSend['id'] = user['id']
             userSend['username'] = user['username']
-            print(user.get('profile_pic'),flush=True)
-            if user.get('profile_pic') != None:
-                userSend['profilePic'] = "./static/images/" + user['profile_pic']
+            if user.get('profilePic') != None:
+                userSend['profilePic'] = user['profilePic']
             else:
                 userSend['profilePic'] = defaultPicture
             userSend['description'] = user.get('description', '')
@@ -43,7 +33,6 @@ def usersHandler():
     # print(1)
     # print(toSend)
     return render_template("users.html", users = toSend)
-
 @usersGiver.route('/allUsers')
 def allUsers():
     users = get_all_users() 
@@ -67,15 +56,31 @@ def singleUser():
     user = userCollection["username"]
     user = json.dumps(user)
     return user
-
 @usersGiver.route('/handleMessage', methods = ["POST"])
 def handleMessageForm():
-    return
+    print(request.data)
+    message = json.loads(request.data)['message']
+    id = session['id']
+    idToSend = messageTracking[id]
+    userData = get_user_from_id(id)
+    username = userData['username']
+    add_message(id, idToSend, username, message)
+    messages = fetch_messages(id, idToSend)
+    print("messages handle: ",messages)
+    print(1)
+    dataToSend = {'id':id, 'idToMessage': idToSend, 'messages': messages}
+    return json.dumps(dataToSend)
 
 @usersGiver.route('/fetchMessages', methods = ["POST"])
 def fetchMessages():
-    idToMessage = request.data
-    dataToSend = {'id':session['id'], 'idToMessage': idToMessage}
-    print(dataToSend)
-    # print('abcd')
-    return json.dumps({'id':session['id'], 'idToMessage': idToMessage})
+    # print(request.data)
+    idToMessage = json.loads(request.data)['id']
+    if session.get('id'):
+        messages = fetch_messages(session['id'], idToMessage)
+        dataToSend = {'id':session['id'], 'idToMessage': idToMessage, 'messages': messages}
+        messageTracking[session['id']] = idToMessage
+        print(dataToSend)
+        print('abcd')
+        return json.dumps(dataToSend)
+    else:
+        return json.dumps({'id':-1})
